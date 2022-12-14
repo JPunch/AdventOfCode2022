31cdc15eb20d8046c8af8ec4bdf27340f8f9990e
Jack Winstanley-Punch <jack.winstanley-punch@cambridgeconsultants.com>
2022-12-14 11:39:44 +0000

    day13 updated parsing and added list recursing

--- day13/day13.py
+++ day13/day13.py
@@ -1,17 +1,31 @@
 import typing as t
 
+
 def get_input(filename: str) -> t.List[str]:
     with open(filename, "r") as f:
-        data = f.readlines()
-    data = [line.strip() for line in data]
+        data = f.read()
     return data
 
 
-def parse_input( input_data: t.List[str], ) -> t.List[str]:
-    return input_data
+def parse_input(input_data: t.List[str]) -> t.List:
+    parsed = []
+    pairs = [i.strip() for i in input_data.split("\n\n")]
+    for pair in pairs:
+        left, right = pair.split("\n")
+        parsed.append((eval(left), eval(right)))
+    return parsed
 
-def day1(input: t.List[str]) -> int:
+
+def day1(input: t.List) -> int:
     return 0
+
+
+def list_recurse(ls: t.List):
+    for element in ls:
+        if isinstance(element, list):
+            return list_recurse(element)
+        else:
+            element
 
 
 def day2(input: t.List[str]) -> int:
@@ -19,7 +33,7 @@ def day2(input: t.List[str]) -> int:
 
 
 def main():
-    input = get_input("day12/input.txt")
+    input = get_input("day13/input.txt")
     parsed_input = parse_input(input)
     day1_result = day1(parsed_input)
     day2_result = day2(parsed_input)
