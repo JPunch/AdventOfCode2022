c74e39c612485d7d4a750db8c32adbebd2e3d2d4
Jack Winstanley-Punch <jackwp@Jacks-MacBook-Pro.local>
2022-12-21 19:26:59 +0000

    day13 part2

--- day13/day13.py
+++ day13/day13.py
@@ -1,6 +1,7 @@
+import typing as t
+from copy import deepcopy
 from enum import Enum
 from functools import cmp_to_key
-import typing as t
 
 
 class ComparisonOutcome(Enum):
@@ -24,10 +25,15 @@ def parse_input(input_data: t.List[str]) -> t.List:
     return parsed
 
 
+def parse_input_day2(input_data: t.List[str]) -> t.List:
+    parsed = [eval(i.strip()) for i in input_data.split("\n") if i != ""]
+    return parsed
+
+
 def day1(input_data: t.List) -> int:
     indexs = []
     for index, val in enumerate(input_data):
-        if compare(*val) == ComparisonOutcome.Right:
+        if compare(*val) > 0:
             indexs.append(index + 1)
     return sum(indexs)
 
@@ -43,12 +49,12 @@ def compare(left, right):
         return compare_element(left, right)
 
 
-def compare_element(left: int, right: int) -> ComparisonOutcome:
+def compare_element(left: int, right: int):
     if left > right:
-        return ComparisonOutcome.Left
+        return -1
     elif left < right:
-        return ComparisonOutcome.Right
-    return ComparisonOutcome.Neither
+        return 1
+    return 0
 
 
 def compare_list(left, right):
@@ -56,25 +62,44 @@ def compare_list(left, right):
     for i in range(len(right)):
         # Check the value, if equal continue looping
         if i >= left_len:
-            return ComparisonOutcome.Right
-        if (ret := compare(left[i], right[i])) != ComparisonOutcome.Neither:
+            return 1
+        if (ret := compare(left[i], right[i])) != 0:
             return ret
     # if it reaches here, all must be equal for length of right
     if left_len > right_len:
-        return ComparisonOutcome.Left
-    return ComparisonOutcome.Neither
+        return -1
+    return 0
 
 
 def day2(input_data: t.List) -> int:
     input_data.extend([[[2]], [[6]]])
-    sorted(input_data, key=cmp_to_key(compare))
-    return 0
+    a = deepcopy(input_data)
+    changes = 0
+    # sorted(a, key=cmp_to_key(compare))
+    sorting_complete = False
+    while not sorting_complete:
+        for i in range(len(a) - 1):
+            if (ret := compare(a[i], a[i + 1])) > 0:
+                # in the correct order
+                pass
+            elif ret < 0:
+                # in reverse order
+                a[i], a[i + 1] = a[i + 1], a[i]
+                changes += 1
+            else:
+                ...
+        if changes == 0:
+            sorting_complete = True
+        else:
+            changes = 0
+    return (a.index([[2]]) + 1) * (a.index([[6]]) + 1)
 
 
 def main():
-    input = get_input("day13/input2.txt")
+    input = get_input("day13/input.txt")
     parsed_input = parse_input(input)
-    day1_result = day1(parsed_input)
+    day1_result = 0  # day1(parsed_input)
+    parsed_input = parse_input_day2(input)
     day2_result = day2(parsed_input)
     print(day1_result, day2_result)
 
