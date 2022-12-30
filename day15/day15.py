86015f307239128a6d335cb84e3d0b7f5ec61f3f
Jack Winstanley-Punch <jackwp@Jacks-MacBook-Pro.local>
2022-12-30 12:29:52 +0000

    day15 part1

--- day15/day15.py
+++ day15/day15.py
@@ -1,5 +1,38 @@
-import typing as t
 import copy
+import re
+import typing as t
+from itertools import combinations_with_replacement
+
+
+class Sensor:
+    def __init__(self, coord: t.Tuple[int, int], b_coord: t.Tuple[int, int]):
+        self.coord = coord
+        self.b_coord = b_coord
+        self.manhattan_distance = Sensor.manhattan_dictance_from_coord(coord, b_coord)
+
+    def coord_in_range(self, coord: t.Tuple[int, int]) -> bool:
+        if (
+            Sensor.manhattan_dictance_from_coord(self.coord, coord)
+            <= self.manhattan_distance
+        ):
+            return True
+        return False
+
+    @staticmethod
+    def manhattan_dictance_from_coord(start_coord, end_coord):
+        return abs(start_coord[0] - end_coord[0]) + abs(start_coord[1] - end_coord[1])
+
+
+def is_sensor_in_y_range(sensor: t.Type["Sensor"], y_value: int) -> bool:
+    if (
+        (sensor.coord[1] < y_value)
+        and (sensor.coord[1] + sensor.manhattan_distance >= y_value)
+    ) or (
+        (sensor.coord[1] > y_value)
+        and (sensor.coord[1] - sensor.manhattan_distance <= y_value)
+    ):
+        return True
+    return False
 
 
 def get_input(filename: str) -> t.List[str]:
@@ -10,15 +43,70 @@ def get_input(filename: str) -> t.List[str]:
     return data
 
 
-def parse_input(input_data: t.List[str]) -> t.List:
-    return input_data
+def parse_input(input_data: t.List[str]) -> t.List[t.Type["Sensor"]]:
+    line_re = re.compile(
+        r"^Sensor at x=(?P<sensor_x>\d+|-\d+),\sy=(?P<sensor_y>\d+|-\d+).*x=(?P<beacon_x>\d+|-\d+), y=(?P<beacon_y>\d+|-\d+)$"
+    )
+    sensors = map(
+        lambda x: Sensor(
+            (int(x.groups()[0]), int(x.groups()[1])),
+            (int(x.groups()[2]), int(x.groups()[3])),
+        ),
+        [line_re.match(line) for line in input_data],
+    )
+    return list(sensors)
 
 
-def day1(grid: t.List) -> int:
-    return 0
+def day1(sensors: t.List[t.Type["Sensor"]]) -> int:
+    """
+    numbers are way too big for a grid to be reasonable
+    Keep track of max x and max y
+    need a good way of populating all points with a manhattan distance = to sensor manhattan distance, maybe combinations or permutations
+    ALTERNATELY: I can check each coord compared to the 10 sensors, just if greater manhattan for all then increase counter
+    """
+    y = 2_000_000
+    # coords = set()
+    # for sensor in sensors:
+    #     coords.add(sensor.coord)
+    #     coords.add(sensor.b_coord)
+    #     for coord in combinations_with_replacement(
+    #         [
+    #             i
+    #             for i in range(
+    #                 -sensor.manhattan_distance, sensor.manhattan_distance + 1
+    #             )
+    #         ],
+    #         2,
+    #     ):
+    #         coords.add((sensor.coord[0] + coord[0], sensor.coord[1] + coord[1]))
+    min_x = sensors[0].coord[0]
+    max_x = sensors[0].coord[0]
+    count = 0
+    for sensor in sensors:
+        if sensor.coord[0] - sensor.manhattan_distance < min_x:
+            min_x = sensor.coord[0] - sensor.manhattan_distance
+        if sensor.b_coord[0] < min_x:
+            min_x = sensor.b_coord[0]
+        if sensor.coord[0] + sensor.manhattan_distance > max_x:
+            max_x = sensor.coord[0] + sensor.manhattan_distance
+        if sensor.b_coord[0] > max_x:
+            max_x = sensor.b_coord[0]
+    sensors_in_range = list(filter(lambda x: is_sensor_in_y_range(x, y), sensors))
+    beacons = [sensor.b_coord for sensor in sensors_in_range]
+    coords = [
+        (x_coord, y)
+        for x_coord in range(min_x, max_x + 1)
+        if (x_coord, y) not in beacons
+    ]
+    for coord in coords:
+        for sensor in sensors_in_range:
+            if sensor.coord_in_range(coord):
+                count += 1
+                break
+    return count
 
 
-def day2(grid: t.List) -> int:
+def day2(sensors: t.List[t.Type["Sensor"]]) -> int:
     return 0
 
 
